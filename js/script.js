d19b878ffcc9b0b0d05de70dda1db47bb8f6df48
Vinod Chandra <98041742+Vinod3d@users.noreply.github.com>
2022-07-29 10:15:23 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const typingText = document.querySelector(".typing-text");
+
+function randomParagraph(){
+    //getting random number and it'll always less than the paragraph length
+    let randIndex = Math.floor(Math.random() * paragraph.length);
+    console.log(randIndex);
+}
+
+randomParagraph();
